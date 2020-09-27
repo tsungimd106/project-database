7fbe88054d14e03f5c6d9519e37da7f094f7b1de
yusiang <10656010@ntub.edu.tw>
2020-09-27 19:40:14 +0800

    調整 - 連接資料庫方式

--- politican.py
+++ politican.py
@@ -1,5 +1,10 @@
 from db import DB
 
-data=DB.execution("select * from area")   
-for (areaid,name) in data:
-    print(areaid,str(name,'utf-8'))
+def create(data):
+
+    sqlstr=("insert into politician(term,name,sex,experience,tel,degree,address) values('%s','%s','%s','%s','%s','%s','%s')" %(
+    data["term"],data["name"],data["sex"],data["experience"],data["tel"],data["degree"],data["addr"]))
+    print(sqlstr)
+    DB.execution(DB.create,sqlstr)
+           
+    
